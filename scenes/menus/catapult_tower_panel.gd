818ac592ba7ac5ef9529b846c2dcd52076572cb6
Justas <justas@digitalguest.com>
2023-08-06 14:12:55 +0200

    [gameplay, adding towers], tower placement uses resources and cannot be placed inside the tower selection UI

--- scenes/menus/catapult_tower_panel.gd
+++ scenes/menus/catapult_tower_panel.gd
@@ -4,9 +4,14 @@ extends Panel
 @onready var tower = preload("res://scenes/actors/catapult_tower/catapult_tower.tscn")
 var currentTower
 
-func _on_gui_input(event):
-	
+func _on_gui_input(event:InputEventMouse):
 	var tempTower = tower.instantiate()
+	var towerDropCordinates = event.global_position
+	var validPosition = true
+	
+	if towerDropCordinates.x < 235 and towerDropCordinates.y > 400:
+		validPosition = false
+	
 	if event is InputEventMouseButton and event.button_mask == 1:
 		add_child(tempTower)
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
@@ -16,8 +21,12 @@ func _on_gui_input(event):
 		
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		get_child(1).queue_free()
-		var addTowerToPath = get_tree().get_root().get_node("main/worldmap/Towers")
+		var main = get_tree().get_root().get_node("main")		
+		var addTowerToPath = main.get_node("worldmap/Towers")
 		
-		addTowerToPath.add_child(tempTower)
-		tempTower.global_position = event.global_position
-		tempTower.get_node("ShootArea").hide()
+		if validPosition and main.resources >= 10:
+			addTowerToPath.add_child(tempTower)
+			tempTower.global_position = event.global_position
+			tempTower.get_node("ShootArea").hide()
+			
+			main.spend_resources(10)
